9616e5fdc65b916780ad59fbad994ff50f2fd7b0
Andrew Huntington <andrew.huntington@gmail.com>
2022-02-28 12:54:06 +0900

    App now generates 10 individual Die components, each with a randomly generated number from 1 to 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Die from "./Die";
 import "./App.css";
 
 function App() {
+  const [dice, setDice] = useState(allNewDice());
+
+  function allNewDice() {
+    let diceArray = new Array(10).fill(1);
+    return diceArray.map((die) => die * Math.floor(Math.random() * 6) + 1);
+  }
+
   return (
     <main className="bg-[#f5f5f5] w-[320px] h-[320px] mx-auto rounded-md flex justify-center items-center mt-10">
       <div className="grid grid-cols-5 gap-4">
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={5} />
-        <Die value={4} />
-        <Die value={1} />
-        <Die value={3} />
-        <Die value={6} />
-        <Die value={3} />
-        <Die value={2} />
-        <Die value={1} />
+        {dice.map((die, index) => (
+          <Die value={die} key={index} />
+        ))}
       </div>
     </main>
   );
--- src/Die.tsx
+++ src/Die.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Die({ value }: { value: number }) {
   return (
-    <div className="bg-white h-9 w-9 flex items-center justify-center rounded shadow">
+    <div className="bg-white h-9 w-9 flex items-center justify-center rounded shadow font-bold text-xl cursor-pointer">
       {value}
     </div>
   );
